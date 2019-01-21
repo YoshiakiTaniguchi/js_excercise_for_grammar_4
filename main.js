b28b974014358176accc0597bebd915b1f97750a
YoshiakiTaniguchi <tanitani717@outlook.com>
2019-01-21 22:32:41 +0900

    third commit

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ for(let i = 1; i < 100; i++) {
     if(i % 5 === 0 && i % 3 === 0) {
         console.log("FizzBuzz" , i);
     }
-    else if((i % 3) === 0 ) {
+    else if(i % 3 === 0 ) {
         console.log("Fizz" , i);
     }
-    else if((i % 5) === 0 ) {
+    else if(i % 5 === 0 ) {
         console.log("Buzz" , i);
     }
 }
